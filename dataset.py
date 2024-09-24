60cbe20a35ba4ceb3204f8dbbc81522edb8c0990
CodeKnight314 <tang.ri@northeastern.edu>
2024-09-24 22:19:37 +0100

    Minor Syntax Changes

--- dataset.py
+++ dataset.py
@@ -17,15 +17,12 @@ class HPEDataset(Dataset):
         super().__init__()
         self.patch_size = patch_size
         
-        # Load annotations
         annFile = os.path.join(root_dir, "annotations", f"person_keypoints_{mode}2017.json")
         with open(annFile, 'r') as file: 
             self.annotations = json.load(file)
         
-        # Build image_id to image info mapping
         self.image_id_to_info = {img['id']: img for img in self.annotations['images']}
         
-        # Build image_id to list of annotations mapping
         self.annotations_per_image = defaultdict(list)
         for ann in self.annotations['annotations']:
             self.annotations_per_image[ann['image_id']].append(ann)
@@ -45,24 +42,19 @@ class HPEDataset(Dataset):
         return len(self.img_ids)
 
     def __getitem__(self, index : int) -> List[torch.Tensor, Dict[str : torch.Tensor]]:
-        # Get image ID and image info
         img_id = self.img_ids[index]
         img_info = self.image_id_to_info[img_id]
         file_name = img_info['file_name']
         width, height = img_info['width'], img_info['height']
         
-        # Load image
         img_path = os.path.join(self.img_dir, file_name)
         img = Image.open(img_path).convert("RGB")
         
-        # Resize image
         img = img.resize((self.patch_size, self.patch_size), Image.BICUBIC)
         
-        # Calculate scaling factors
         scale_x = self.patch_size / width
         scale_y = self.patch_size / height
         
-        # Get annotations for the image
         annotations = self.annotations_per_image[img_id]
         
         keypoints_list = []
@@ -73,21 +65,17 @@ class HPEDataset(Dataset):
         
         for ann in annotations:
             if ann['num_keypoints'] > 0:
-                # Process keypoints
-                keypoints = np.array(ann['keypoints']).reshape(-1, 3)
+                keypoints = np.array(ann['keypoints']).reshape(-1, 2)
                 
-                # Scale keypoints
                 keypoints[:, 0] *= scale_x
                 keypoints[:, 1] *= scale_y
                 keypoints_list.append(keypoints)
                 
-                # Process bounding boxes
-                bbox = ann['bbox']  # [x_min, y_min, width, height]
+                bbox = ann['bbox']
                 x_min, y_min, box_width, box_height = bbox
                 x_max = x_min + box_width
                 y_max = y_min + box_height
                 
-                # Scale bounding boxes
                 x_min *= scale_x
                 y_min *= scale_y
                 x_max *= scale_x
@@ -99,20 +87,18 @@ class HPEDataset(Dataset):
                 iscrowd.append(ann['iscrowd'])
         
         if len(keypoints_list) == 0:
-            # If no valid annotations, create empty tensors
             keypoints = torch.zeros((0, 17, 3), dtype=torch.float32)
             boxes = torch.zeros((0, 4), dtype=torch.float32)
             labels = torch.zeros((0,), dtype=torch.int64)
             areas = torch.zeros((0,), dtype=torch.float32)
             iscrowd = torch.zeros((0,), dtype=torch.int64)
         else:
-            keypoints = torch.tensor(keypoints_list, dtype=torch.float32)  # (N, 17, 3)
-            boxes = torch.tensor(boxes_list, dtype=torch.float32)          # (N, 4)
+            keypoints = torch.tensor(keypoints_list, dtype=torch.float32)  
+            boxes = torch.tensor(boxes_list, dtype=torch.float32)          
             labels = torch.tensor(labels, dtype=torch.int64)
             areas = torch.tensor(areas, dtype=torch.float32)
             iscrowd = torch.tensor(iscrowd, dtype=torch.int64)
         
-        # Data preparation
         img = self.img_transform(img)
         
         target = {
@@ -126,7 +112,6 @@ class HPEDataset(Dataset):
         
         return img, target
 
-# For converted COCO dataset with only one HPE target per sample
 class HPESingle(Dataset): 
     def __init__(self, root_dir : str, mode : str, patch_size : int): 
         super().__init__() 
@@ -146,20 +131,16 @@ class HPESingle(Dataset):
         return len(self.img_dir)
     
     def __getitem__(self, index):
-        # Load image 
         img = Image.open(self.img_dir[index]).convert("RGB")
         width, height = img.size
         
-        # Determine scale width and height for img pose coordinates
         scale_width, scale_height = self.patch_size / width, self.patch_size / height
 
-        # Open corresponding annotation txt and scale x, y positione
         with open(self.ann_dir[index], 'r') as f:
             data = [[float(x) for x in line.strip().split()] for line in f]
             data[:, 0] *= scale_width
             data[:, 1] *- scale_height
         
-        # Convert data to tensors  
         img_tensor = self.transform(img)
         ann_tensor = torch.Tensor(data)
         
